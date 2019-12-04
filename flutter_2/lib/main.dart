82fbf8afeaca26c76433512ddb6e68d15222c3eb
zhaochao <zhaochao@rd.netease.com>
2019-12-04 16:36:55 +0800

    add

--- flutter_2/lib/main.dart
+++ flutter_2/lib/main.dart
@@ -16,9 +16,107 @@ void main() => runApp(
     title: 'my app',
     //home: new MyScaffold(),
     //home: new TunoroalHome(),
-    home: new Counter(),
+    //home: new Counter(),
+    home: new ShoppingList(
+      products: [
+        new Product(name: 'eggs'),
+        new Product(name: 'flour'),
+        new Product(name: 'fish'),
+      ],
+    ),
   ),
 );
+
+
+class Product {
+  const Product({this.name});
+  final String name;
+}
+
+typedef void CartChangeeCallback(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget{
+  ShoppingListItem({Product product, this.inCart, this.onCartChanged}) : product = product, super(key: new ObjectKey(product));
+
+  final Product product;
+  final bool inCart;
+  final CartChangeeCallback onCartChanged;
+
+
+  Color _getColor(BuildContext context){
+    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle _getTextStyle(BuildContext context){
+    if(!inCart) return null;
+    return new TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+     return new ListTile(
+       onTap: (){
+         onCartChanged(product, !inCart);
+         print('${product.name}, $inCart');
+       },
+       leading: new CircleAvatar(
+         backgroundColor: _getColor(context),
+         child: new Text(product.name[0]),
+       ),
+       title: new Text(product.name, style:_getTextStyle(context)),
+     );
+  }
+}
+
+
+class ShoppingList extends StatefulWidget{
+  ShoppingList({Key key, this.products}) : super(key: key);
+
+  final List<Product> products;
+  @override
+  State<StatefulWidget> createState() {
+    return new _ShoppingListState();
+  }
+}
+
+class _ShoppingListState extends State<ShoppingList>{
+    Set<Product> _shoppingCart = new Set<Product>();
+
+    void _handleCartChanged(Product product, bool inCart){
+        setState(() {
+          if(inCart){
+             _shoppingCart.add(product);
+          }else{
+            _shoppingCart.remove(product);
+          }
+        });
+    }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('商品列表'),
+      ),
+      body: new ListView(
+        padding: new EdgeInsets.symmetric(vertical: 8.0),
+        children: widget.products.map((Product product){
+              return new ShoppingListItem(
+                 product: product,
+                 inCart: _shoppingCart.contains(product),
+                 onCartChanged: _handleCartChanged,
+              );
+          }).toList(),
+      )
+    );
+  }
+}
+
+
 
 //有状态widget
 class Counter extends StatefulWidget{
